00561081eed785081d22cb9fec5dd699f459e8d6
william <williamasauer@gmail.com>
2020-08-25 09:56:17 -0600

    Stuff

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main() //Entry point  
+    {
+    }
+
+    static void Main(String[] args) //another entrypoint!!! Compile Error  
+    {
+        Console.WriteLine("Debuging");
+    }
+}
---
+++ Math stuff/Project2/Math project1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Math_project1
+{
+    class Program
+    {
+        static string Number1 = Console.ReadLine();
+        static string Number2 = Console.ReadLine();
+        static string Number3 = Console.ReadLine();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Number1);
+            Console.WriteLine(Number2);
+            Console.WriteLine(Number3);
+
+        }
+    }
+}
