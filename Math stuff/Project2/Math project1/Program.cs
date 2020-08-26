e05042c548150e6cfa7503985028b24f03a4a10f
william <williamasauer@gmail.com>
2020-08-26 11:43:45 -0600

    Got the x thing working

--- Math stuff/Project2/Math project1/Program.cs
+++ Math stuff/Project2/Math project1/Program.cs
@@ -1,18 +1,51 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Math_project1
 {
     class Program
     {
-        static string Number1 = Console.ReadLine();
-        static string Number2 = Console.ReadLine();
-        static string Number3 = Console.ReadLine();
+        
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Number1);
-            Console.WriteLine(Number2);
-            Console.WriteLine(Number3);
+
+            string numbers = Console.ReadLine();
+
+            string[] words = numbers.Split(' ');
+
+            
+            foreach (var word in words)
+            {
+                int x = word.IndexOf("x");
+
+
+                System.Console.WriteLine("X = " + x);
+                if (x == -1)
+                {
+                    System.Console.WriteLine($"{word}");
+
+                }
+
+                if (x >= 1)
+                {
+
+
+                    int substringsub = word.Length - 1;
+                    int substringsubs = word.Length;
+                    string sub = word.Substring(0, x);
+                    string subs = word.Substring(x);
+
+
+                    Console.WriteLine(sub);
+                    Console.WriteLine(subs);
+
+
+
+
+                }
+
+            }
 
         }
     }
