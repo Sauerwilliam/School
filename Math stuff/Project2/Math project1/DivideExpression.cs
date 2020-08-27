90429a0214ab8a5d5e1e2149f9038aafd56a3a40
william <williamasauer@gmail.com>
2020-08-27 11:22:04 -0600

    Hi

---
+++ Math stuff/Project2/Math project1/DivideExpression.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_project1
+{
+    class DivideExpression : Expression
+    {
+        private Expression left;
+        private Expression right;
+        public DivideExpression(Expression newLeft, Expression newRight)
+        {
+            left = newLeft;
+            right = newRight;
+        }
+            
+        public override int evaluate(int x)
+        {
+            return left.evaluate(x) / right.evaluate(x);
+        }
+
+        public override void printMe(int depth)
+        {
+            Console.WriteLine("-- /");
+            left.print(depth + 1);
+            right.print(depth + 1);
+        }
+    }
+}
---
+++ Math stuff/Project2/Math project1/MultiplyExpression.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_project1
+{
+    class MultiplyExpression : Expression
+    {
+        public MultiplyExpression(Expression newLeft, Expression newRight)
+        {
+            left = newLeft;
+            right = newRight;
+        }
+        private Expression left;
+        private Expression right;
+        public override int evaluate(int x)
+        {
+            return left.evaluate(x) * right.evaluate(x);
+        }
+
+        public override void printMe(int depth)
+        {
+            Console.WriteLine("-- *");
+            left.print(depth + 1);
+            right.print(depth + 1);
+        }
+    }
+}
